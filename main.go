ec34636b4893d899664953d5a7abb41ee3bfc8c7
Yevhen Hrytsai <yevgen.grytsay@gmail.com>
2024-09-03 10:07:41 +0300

    detect request logs

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"regexp"
+	"strings"
 
 	"github.com/nxadm/tail"
 )
@@ -10,10 +11,16 @@ import (
 func main() {
 	// Create a tail
 	t, err := tail.TailFile(
-		"/var/log/nginx/error.log", tail.Config{Follow: true, ReOpen: true})
+		"/var/log/nginx/error.log",
+		// tail.Config{Follow: true, ReOpen: true},
+		tail.Config{Follow: false},
+	)
 	if err != nil {
 		panic(err)
 	}
+
+	var logSequenceMap = make(map[string]LogSequence)
+	var lastLogLine *LogLine
 
 	// Print the text of each received line
 	for line := range t.Lines {
@@ -21,11 +28,29 @@ func main() {
 
 		parsedLogLine, error := parseLogLine(line.Text)
 		if error != nil {
-			fmt.Printf("ERROR: %s\n", error)
+			// fmt.Printf("ERROR: %s\n", error)
 			continue
 		}
 
+		lastLogLine = parsedLogLine
 		fmt.Printf("%#v\n", parsedLogLine)
+
+		item, ok := logSequenceMap[lastLogLine.hash()]
+		if !ok {
+			item = LogSequence{PidAndTid: parsedLogLine.PidAndTid, RequestId: parsedLogLine.RequestId}
+		}
+		item.push(parsedLogLine)
+		/* if item.isComplete() {
+			fmt.Printf("Sequence completed %s\n", item.PidAndTid)
+			fmt.Printf("%#v", item)
+
+			os.Exit(0)
+		} */
+		logSequenceMap[lastLogLine.hash()] = item
+	}
+
+	for key, value := range logSequenceMap {
+		fmt.Println("Key:", key, "Hash:", value.PidAndTid, "_", value.RequestId, "IsComplete:", fmt.Sprintf("%#v", value.isComplete()))
 	}
 }
 
@@ -38,13 +63,43 @@ const (
 )
 
 type LogLine struct {
-	LogLevel LogLevel
-	Message  string
+	LogLevel  LogLevel
+	Message   string
+	PidAndTid string
+	RequestId string
+}
+
+func (l LogLine) hash() string {
+	return fmt.Sprintf("%s_%s", l.PidAndTid, l.RequestId)
+}
+
+type LogSequence struct {
+	PidAndTid    string
+	RequestId    string
+	lines        []LogLine
+	hasStartLine bool
+	hasEndLine   bool
+}
+
+func (s *LogSequence) push(line *LogLine) {
+	s.lines = append(s.lines, *line)
+
+	if strings.HasPrefix(line.Message, "http process request line") {
+		s.hasStartLine = true
+	}
+
+	if strings.HasPrefix(line.Message, "http filename:") {
+		s.hasEndLine = true
+	}
+}
+
+func (s LogSequence) isComplete() bool {
+	return s.hasEndLine && s.hasStartLine
 }
 
 func parseLogLine(text string) (*LogLine, error) {
 	// re := regexp.MustCompile(`(?P<Date>\d{4}/\d{2}/\d{2}) (?P<Time>\d{2}:\d{2}:\d{2}) \[(?P<Level>\w+)\] \d+#\d+: \*\d+ (?P<Message>.*?), client: (?P<ClientIP>[\d\.]+), server: (?P<ServerName>[^,]+), request: "(?P<Request>[^"]+)", host: "(?P<Host>[^"]+)"`)
-	re := regexp.MustCompile(`(?P<Date>\d{4}/\d{2}/\d{2}) (?P<Time>\d{2}:\d{2}:\d{2}) \[(?P<Level>\w+)\] \d+#\d+: (?P<Message>.*)`)
+	re := regexp.MustCompile(`(?P<Date>\d{4}/\d{2}/\d{2}) (?P<Time>\d{2}:\d{2}:\d{2}) \[(?P<Level>\w+)\] (?P<PidAndTid>\d+#\d+): \*(?P<RequestID>\d+) (?P<Message>.*)`)
 
 	match := re.FindStringSubmatch(text)
 	if len(match) == 0 {
@@ -56,15 +111,15 @@ func parseLogLine(text string) (*LogLine, error) {
 	result := &LogLine{}
 	for i, name := range groupNames {
 		if i != 0 && name != "" {
-			// if i >= len(match) {
-			// 	fmt.Errorf("Out of bounds: %s", text)
-			// }
-			// fmt.Printf("%s: %s\n", name, match[i])
 			switch name {
 			case "Level":
 				result.LogLevel = LogLevel(match[i])
 			case "Message":
 				result.Message = match[i]
+			case "PidAndTid":
+				result.PidAndTid = match[i]
+			case "RequestID":
+				result.RequestId = match[i]
 			}
 		}
 	}
